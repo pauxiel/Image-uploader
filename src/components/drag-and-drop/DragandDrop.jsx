7ba89d894b2a69ec8efc13f61e9519db83fa61b9
pauxiel <paul.scholes50@yahoo.com>
2022-06-20 12:44:39 +0100

    drag and drop was not working so i corrected it

--- src/components/drag-and-drop/DragandDrop.jsx
+++ src/components/drag-and-drop/DragandDrop.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { useState } from "react";
 import "./drag-drop.scss";
 import logo from "../../assets/images/logo.svg";
 
 function DragandDrop({ uploadImage }) {
+  const [borderColor, setBorderColor] = useState('#2f80ed')
   const handleonDrop = (e) => {
     e.preventDefault();
     const imageFile = e.dataTransfer.files
+    console.log('dropped')
     uploadImage(imageFile['0'])
+    
+    // console.log('dropped')
   };
+
+  function handleOnDragOver(e) {
+    e.preventDefault()
+    setBorderColor('green')
+  }
 
   const handleonDrag = (e) => {
     e.preventDefault();
@@ -15,18 +25,22 @@ function DragandDrop({ uploadImage }) {
 
   const handleonDragLeave = (e) => {
     e.preventDefault();
+  
   };
 
   const handleonDragEnter = (e) => {
     e.preventDefault();
+    setBorderColor('green')
   };
   return (
     <div
       className="dropzone"
+      onDragOver = {handleOnDragOver}
       onDrop={handleonDrop}
       onDrag={handleonDrag}
       onDragLeave={handleonDragLeave}
       onDragEnter={handleonDragEnter}
+      style={{ border: `2px dashed ${borderColor}` }}
     >
       upload your image box with dash lined and iamge inside
       <div className="dropzone__logo">
